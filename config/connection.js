9850595bc5f8c157e48b5aa786bbfb38b1d6485e
Relten98 <diezhandinoty@gmail.com>
2021-02-12 17:54:43 -0800

    updated core files

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "password123"
+    password: "password123",
+    insecureAuth : true
   });
   
-  con.connect(function(err) {
+  connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    console.log("WOOOO! YEEEEEAAAAAH BABY That's what I've been waiting for.");
   });
+
+  module.exports;
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 const express = require("express");
+const mysql = require("mysql")
+
+const birds = 15000
 
 let port = 3080
