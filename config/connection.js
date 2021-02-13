a049766bee66439d9bf8cf1f950702a84194120f
Relten98 <diezhandinoty@gmail.com>
2021-02-12 17:21:12 -0800

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,12 @@
 var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password123"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
