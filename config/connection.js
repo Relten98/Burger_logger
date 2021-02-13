678ac7003ec40ee2be858bbb61727731fd97a2f1
Relten98 <diezhandinoty@gmail.com>
2021-02-12 18:05:54 -0800

    update connection files

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password123",
     insecureAuth : true
   });
   
-  connection.connect(function(err) {
+  con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
     console.log("WOOOO! YEEEEEAAAAAH BABY That's what I've been waiting for.");
