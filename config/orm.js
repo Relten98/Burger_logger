54820b19e6bcd5395f0cb2b157037f819d107de1
Relten98 <diezhandinoty@gmail.com>
2021-02-15 11:20:33 -0800

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,15 @@
 /// Requirements and shiz, lets get that out of the way.
 let con = require('./connection.js');
 
+// This is where the fun begins.
+let orm = {
 
- selectAll() 
+}
 
-
-
+/*
  selectAll()
  insertOne()
  updateOne()
-
+*/
  
 module.exports = orm;
