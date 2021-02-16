d1d2fb530275b848a2de2f34000b17d6ab739cec
Relten98 <diezhandinoty@gmail.com>
2021-02-15 17:09:21 -0800

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,39 @@
 /// Requirements and shiz, lets get that out of the way.
+let maybe = false
+
 let con = require('./connection.js');
 
 // This is where the fun begins.
-let orm = {
+var orm = {
+    selectAll: function (callback) {
+        //This be mySQL Query
+        connection.query('SELECT * FROM burgers', function (err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    },
+    
+    //add a BORGER
+    insertOne: function (burger_name, callback) {
+        connection.query('INSERT INTO burgers SET ?',
+            {
+                // BORGERS
+                burger_name: burger_name,
+                // Should allow user to change it's state later.
+                devoured: false,
+            }, function (err, result) {
+                if (err) throw err;
+                callback(result);
+            });
+    },
+    // Move BORGER
+    updateOne: function (burgerID, callback) {
+        connection.query('UPDATE burgers SET ? WHERE ?', [{ devoured: true }, { id: burgerID }],
+            function (err, result) {
+                if (err) throw err;
+                callback(result);
+            });
+    }
+};
 
-}
-
-/*
- selectAll()
- insertOne()
- updateOne()
-*/
- 
 module.exports = orm;
