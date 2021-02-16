8d5d7c2033932c4fba520ee0fc04e5a97c63813a
Relten98 <diezhandinoty@gmail.com>
2021-02-16 09:19:14 -0800

    updated core files
    Nearing completion on the application

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,31 @@
-// Imports
-let borger = require('./burger.js');
+// Imports the required information.
+let borger = require('./models/burger.js');
 
 let express = require('express');
+
+const express.router();
+// And now for the routers
+
+router.get('/', function (req, res) {
+    res.redirect
+});
+
+router.get('/index', function (req, res) {
+    burger.selectall(function (data) {
+        res.render('index');
+    });
+
+});
+
+router.post('/burger/create', function (req, res) {
+    burger.insertOne(req.body.burger_name, function () {
+        res.redirect('/index');
+    });
+});
+router.post('/burger/eat/:id', function (req, res) {
+    burger.updateOne(req.params.id, function () {
+        res.redirect('/index');
+    });
+});
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,26 @@
-let orm = require('./orm.js');
+// Imports the ORM
+let orm = require('../config/orm.js');
 
+// I hope this works....
+// this be the model
+let borger = {
+    selectAll: function (callback) {
+        orm.selectAll(function (results) {
+            callback(results);
+        });
+    },
 
+    insertOne: function (burger_name, callback) {
+        orm.selectAll(burger_name, function (results) {
+            callback(results);
+        });
+    },
 
+    updateOne: function (burger_id, callback) {
+        orm.selectAll(burger_id, function (results) {
+            callback(results);
+        });
+    }
 
-module.exports = borger;
+// Neatly exports everything.
+module.exports = borger; 
