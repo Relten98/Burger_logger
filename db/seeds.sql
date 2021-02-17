690c2325a310b51c6dbae6e1b38e71361d2913b9
Relten98 <diezhandinoty@gmail.com>
2021-02-16 19:23:57 -0800

    Update seeds.sql
    MemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMeMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesemesMemesMemesMemesMemesemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesmesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemesMemes

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,18 @@ VALUES ("DAIKATANA", TRUE);
 
 INSERT INTO burger (burger_name, devoured)
 VALUES ("Wombo-Combo", FALSE);
+INSERT INTO burger (burger_name, devoured)
+VALUES ("DAIKATANA", TRUE);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Two number 9s", FALSE);
+INSERT INTO burger (burger_name, devoured)
+VALUES ("A number 9 large", FALSE);
+INSERT INTO burger (burger_name, devoured)
+VALUES ("A number 6 with extra dip", FALSE);
+INSERT INTO burger (burger_name, devoured)
+VALUES ("A number 7", FALSE);
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Two number 45s, one with cheese", FALSE);
+INSERT INTO burger (burger_name, devoured)
+VALUES ("A large soda, Diet", FALSE);
