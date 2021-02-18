412ca41e4a7ef5c4dd7d39e9e2c3d06dc4bcfdaf
Relten98 <diezhandinoty@gmail.com>
2021-02-18 15:45:48 -0800

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,29 @@
-var mysql = require('mysql');
+// Set up MySQL connection.
+const mysql = require("mysql");
 
-var exported;
+let connection;
 
-var con = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password123",
-    insecureAuth : true
+    database: "burgers_db"
   });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    console.log("WOOOO! YEEEEEAAAAAH BABY! That's what I've been waiting for.");
-  });
+};
 
-  module.exports = exported;
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+  console.log("Connected!");
+  console.log("WOOOO! YEEEEEAAAAAH BABY! That's what I've been waiting for.");
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
