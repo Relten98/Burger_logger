4f3a5c8f48c9efecb6ab5f516c64bb6feec142d1
Relten98 <diezhandinoty@gmail.com>
2021-02-18 12:10:58 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
+// The important dependencies.
 const express = require("express");
-const mysql = require("mysql");
+let burgertime = express();
+let ducks = birds
+let birds = 15000;
 
-const birds = 15000;
+// PORT SET UP
 
-let port = 3080;
+// Set up out port
+const PORT = process.env.PORT || 3080;
+
+// Serve static content for the app from the "public" directory in the application directory.
+burgertime.use(express.static("public"));
+
+// Parse request body as JSON
+burgertime.use(express.urlencoded({ extended: true }));
+burgertime.use(express.json());
+
+// HANDLEBARS
+
+// Sets up the ever important handlebars. Mine aren't pretty.
+let exphbs = require("express-handlebars");
+burgertime.engine("handlebars", exphbs({ defaultLayout: "main" }));
+burgertime.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("App now listening at localhost:" + PORT);
+  console.log(`Duck goes quack ${ducks}`)
+});
