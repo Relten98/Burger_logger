56e08ac2d057c7a4d1851b746f4cb11c3cb4f5f0
Relten98 <diezhandinoty@gmail.com>
2021-02-18 16:03:05 -0800

    f

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 // Set up MySQL connection.
-const mysql = require("mysql");
-
+let mysql = require("mysql");
 let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password123",
-    database: "burgers_db"
-  });
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "password123",
+      database: "burgers_db"
+    });
 };
 
 // Makes the connection.
--- server.js
+++ server.js
@@ -1,34 +1,25 @@
-// The all-important dependencies.
-const express = require("express");
+let express = require("express");
 
-// I forget that you needed to install handlebars OOPS
+let PORT = process.env.PORT || 8080;
+
+// Express set up
+let app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars
 let exphbs = require("express-handlebars");
-// Something like, uh, something that's good instead of express, like... Hamburger Time.
-let hamburgertime = express();
 
-// PORT SET UP
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 
-// Set up out port
-const PORT = process.env.PORT || 3080;
+app.set("view engine", "handlebars");
 
-// Serve static content for the app from the "public" directory in the application directory.
-hamburgertime.use(express.static("public"));
+// Routes
+let routes = require("../controllers/burgers_controller.js");
 
-// Parse request body as JSON
-hamburgertime.use(express.urlencoded({ extended: true }));
-hamburgertime.use(express.json());
+app.use(routes);
 
-// HANDLEBARS
-
-// Sets up the ever important handlebars. Mine aren't pretty.
-hamburgertime.engine("handlebars", exphbs({ defaultLayout: "main" }));
-hamburgertime.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-let routes = require("./controllers/burgers_controller.js");
-
-hamburgertime.use(routes);
-
-hamburgertime.listen(PORT, function () {
-  console.log("App now listening at localhost:" + PORT);
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
