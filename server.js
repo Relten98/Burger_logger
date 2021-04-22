3ce0797f8b298fde034cf579b8c8610e7305e9be
Relten98 <diezhandinoty@gmail.com>
2021-04-22 12:17:22 -0700

    update

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const routes = require("./controllers/burgers_controller.js");
 const exhandlbars = require("express-handlebars");
 
+let PORT = process.env.PORT || 3000;
+
 // For express
 const burgertime = express();
-
-let PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 burgertime.use(express.static("public"));
@@ -15,11 +15,16 @@ burgertime.use(express.json());
 
 // Set Handlebars.
 burgertime.engine("handlebars", exhandlbars({ defaultLayout: "main" }));
+
+
 burgertime.set("view engine", "handlebars");
 
 
 // Import routes and give the server access to them.
 burgertime.use(routes);
+
+
+
 burgertime.listen(PORT, function () {
   console.log("App now listening at localhost:" + PORT);
 });
