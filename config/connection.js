324547f4b0678eea840246423d13453fc6503825
Relten98 <diezhandinoty@gmail.com>
2021-04-26 19:58:26 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,24 @@
 // Set up MySQL connection.
 let mysql = require("mysql");
+let connection;
+// Main  connection
+if (process.env.JAWSDB_URL) {
+     connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+     connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306, // note-to-self: database will not connect with 3000
+        user: "root",
+        password: "password123",
+        database: "burgers_db"
+    });
+}
 
-// Main connection
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306, // note-to-self: database will not connect with 3000
-    user: "root",
-    password: "password123",
-    database: "burgers_db"
-});
+
+
 
 
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
+connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
